e0ae9df4b64e37d377f1051c9dc28f051a1472a5
Syafiqah Azwani <nsyafiqaha96@gmail.com>
2025-03-18 08:18:12 +0800

    Chat-Application

---
+++ app/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    id("com.google.gms.google-services")
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.chat_app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.chat_app"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        multiDexEnabled = true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.firebase.messaging)
+    implementation(libs.firebase.firestore)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    // Scalable Size Unit (support for different screen sizes)
+    implementation("com.intuit.sdp:sdp-android:1.0.6")
+    implementation("com.intuit.ssp:ssp-android:1.0.6")
+
+    // Rounded ImageView
+    implementation("com.makeramen:roundedimageview:2.3.0")
+
+    // MultiDex
+    implementation("androidx.multidex:multidex:2.0.1")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+
+}
+
+
+
+
+
+
+
+
+
